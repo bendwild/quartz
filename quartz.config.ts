b50d59f70f1f7032c21eca0a6119934b1c785ec6
bendwild <dewildebenjamin0@gmail.com>
2026-02-10 17:02:33 -0800

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "hi cutie pie",
+    pageTitle: "Hi cutie pie",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
